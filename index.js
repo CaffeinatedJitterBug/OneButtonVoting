d05e0200ba55533f532c7000b338e825842fbeda
Amy Green <itwallaby@gmail.com>
2025-01-16 19:11:59 -0500

    Fixed invisible links

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function getId(id){
     id.toString();
     let links = document.getElementsByClassName(id);
     for (let x = 0; x < links.length; x++){
+        links[x].setAttribute("onclick", "true");
         links[x].click();
+        links[x].setAttribute("onclick", "false");
     }
 };
